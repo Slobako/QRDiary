a66a081f1d0c82c3c8ab0375b2bc5f9f832df3da
Slobako <slobkov02@yahoo.com>
2016-05-09 15:59:17 -0400

    Initial Commit

---
+++ QRDiary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QRDiary
+//
+//  Created by Slobodan Kovrlija on 5/9/16.
+//  Copyright © 2016 Slobodan Kovrlija. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QRDiary/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  QRDiary
+//
+//  Created by Slobodan Kovrlija on 5/9/16.
+//  Copyright © 2016 Slobodan Kovrlija. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
