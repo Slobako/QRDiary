b31c5aa86c9d2bffdaf330a4ec0bf7e688009185
Slobako <slobkov02@yahoo.com>
2016-05-31 19:48:58 -0400

    info VC added

---
+++ QRDiary/QRDInfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  QRDInfoViewController.h
+//  QRDiary
+//
+//  Created by Slobodan Kovrlija on 5/31/16.
+//  Copyright © 2016 Slobodan Kovrlija. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QRDInfoViewController : UIViewController
+
+@end
